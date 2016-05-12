608ff09be547479089d91e7dd3251b96307402ef
nagahama <mavuser@Mac.local>
2016-05-12 18:45:06 +0900

    削除

--- Network_Simulator/php/test.php
+++
@@ -1,8 +0,0 @@
-<!--<?php/*
-  $output = shell_exec('./waf --run examples/tutorial/flrst 2>&1');
-  echo "<pre>テスト</pre>";
-*/?>-->
-<pre>読み込んだ内容。
-年：<?php echo  $_GET['year']; ?>年
-月：<?php echo  $_GET['month']; ?>月
-日：<?php echo  $_GET['day']; ?>日</pre>
