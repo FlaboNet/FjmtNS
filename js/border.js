33fc05241080c97cd59d8f72a77eb4dc311b88cb
hama3 <ccjsj666@yahoo.co.jp>
2016-05-29 21:27:58 +0900

    ある程度のバグの修正

--- js/border.js
+++ js/border.js
@@ -2,17 +2,15 @@
  * ネットワークシミュレータのプログラム
  * LANの動作について
  *
- * do: コードが冗長なので短くしたい
- * bg: 線が引かれた時にクリックした時のエラー(動作の影響度無だが気になる)
- *     画像同士が重なると線がおかしくなる(動作の影響度中)[Z軸の問題]
+ * do:
+ * bg:
+ *
  */
 
 $(function(){
-
   var ctxArr = [];        // キャンバス情報の保存配列
   var lanArr = [];        // LANの座標情報の保存配列
   var nodeInt = 0;        // Nodeの個数
-
   var points = [];        // ドラッグ時のマウスの座標を集める
   var pointsFirst = [];   // ドラッグ開始時のマウスの座標
   var $canvas;            // 追加されたキャンバスを格納
@@ -22,20 +20,11 @@ $(function(){
   var lnkFlag;            // フラグ2
   var lanFlag_point;      // フラグ3
 
-  // 線を描く関数
-  function getRectPoints(ptax, ptay, ptbx, ptby, width) {
-
-    var rad = Math.atan2(ptby - ptay, ptbx - ptax);
-    var offX = (width / 2) * Math.sin(rad);
-    var offY = (width / 2) * Math.cos(rad);
-
+  // 線の座標を整える関数
+  function getRectPoints(ptax, ptay, ptbx, ptby) {
     var array = [];
-
-    array.push({x:ptax + offX, y:ptay - offY});
-    array.push({x:ptbx + offX, y:ptby - offY});
-    array.push({x:ptbx - offX, y:ptby + offY});
-    array.push({x:ptax - offX, y:ptay + offY});
-
+    array.push({x:ptax, y:ptay});
+    array.push({x:ptbx, y:ptby});
     return array;
   }
 
@@ -45,24 +34,22 @@ $(function(){
     ctxArr = [];
     lanArr = [];
     nodeInt = 0;
-
-    //lanLinckがある時
-    if( $("#main img").hasClass("lanLinck") ){
+    // lanLinckがある時
+    if($("#main img").hasClass("lanLinck")){
       $("#main").off("mousedown", lanDown2);
       $("#main").off("mouseup", lanUp2);
     }
-
+    // 画像と線の削除
     $("#main img").remove();
     $("#main canvas").remove();
   });
 
   // class(.lan)のクリック
   $(".lan").click(function(){
-
     // LANが押されているときの動作
     if($(this).attr("src") == "img/lanCable.png"){
       // lanLinckがある時
-      if( $("#main img").hasClass("lanLinck") ){
+      if($("#main img").hasClass("lanLinck")){
         $("#main").off("mousedown", lanDown2);
         $("#main").off("mouseup", lanUp2);
       }
@@ -71,17 +58,11 @@ $(function(){
       $(this).attr("src", "img/lanCable_2.png");
 
       // 画像のドラッグ防止
-      $("#main img").mouseup(function(e){
-        e.preventDefault();
-      });
-      $("#main img").mousedown(function(e){
-        e.preventDefault();
-      });
+      $("#main img").mouseup(function(e) { e.preventDefault(); });
+      $("#main img").mousedown(function(e) { e.preventDefault(); });
 
       // Canvasを追加する関数
-      function addCanvas(){
-        return $('<canvas width="' + canvasWidth + '" height="' + canvasHeight + '"></canvas>').prependTo('#main');
-      }
+      function addCanvas()　{ return $('<canvas width="' + canvasWidth + '" height="' + canvasHeight + '"></canvas>').prependTo('#main');　}
 
       // マウスのボタンが押されたときに処理を実行する関数
       function lanDown(e){
@@ -91,7 +72,7 @@ $(function(){
           $canvas = addCanvas();
           lanFlag_point = true;
           // lanLinckがある場合
-          if ($(this).children(".lanOn").hasClass("lanLinck")){ lnkFlag = true; }
+          if ($(this).children(".lanOn").hasClass("lanLinck"))　{ lnkFlag = true; }
           // Classの追加
           $(this).children(".lanOn").addClass("lanFrist lanLinck sP_"+ nodeInt);
           // マウスを押した場所から線を描画
@@ -105,47 +86,31 @@ $(function(){
       // マウスが移動したときに処理を実行する関数
       function lanMove(e){
         var ctx = $canvas.get(0).getContext('2d');
-
         points.push({x:e.pageX - this.offsetLeft, y:e.pageY - this.offsetTop});
-
-        var array = getRectPoints(
-          points[0].x,                  // 線Xの始点
-          points[0].y,                  // 線Yの始点
-          points[points.length - 1].x,  // 線Xの終点
-          points[points.length - 1].y,  // 線Yの終点
-          2                             // 線の太さ
-        );
 
         ctx.clearRect(0, 0, canvasWidth, canvasHeight);
         ctx.beginPath();
-
         // 線の色の変更
-        if(!($(e.toElement).hasClass("lanFrist")) && $(e.toElement).hasClass("lanOn")) { ctx.fillStyle = '#2fb9fe'; }
-        else { ctx.fillStyle = '#fb9003'; }
-
-        ctx.moveTo(array[0].x, array[0].y);
-        ctx.lineTo(array[1].x, array[1].y);
-        ctx.lineTo(array[2].x, array[2].y);
-        ctx.lineTo(array[3].x, array[3].y);
-        ctx.closePath();
+        if(!($(e.target).hasClass("lanFrist")) && $(e.target).hasClass("lanOn")) { ctx.strokeStyle = '#2fb9fe'; }
+        else { ctx.strokeStyle = '#fb9003'; }
+        ctx.lineWidth = 2;
+        ctx.moveTo(points[0].x, points[0].y);
+        ctx.lineTo(points[points.length - 1].x, points[points.length - 1].y);
+        ctx.stroke();
         ctx.fill();
-
       }
 
       // マウスのボタンが離されたときに処理を実行する関数
       function lanUp(e){
-
         // 画像の真ん中に線を持ってくる動作（コードが冗長なのでできれば改善）
-        if (!($(e.toElement).hasClass("lanFrist")) && $(e.toElement).hasClass("lanOn")){
+        if (!($(e.target).hasClass("lanFrist")) && $(e.target).hasClass("lanOn")){
           var ctx = $canvas.get(0).getContext('2d');
           points.push({x:e.pageX - this.offsetLeft - e.offsetX + 38, y:e.pageY - this.offsetTop - e.offsetY + 38});
-
           var array = getRectPoints(
             pointsFirst[0].x,
             pointsFirst[0].y,
             points[points.length - 1].x,
-            points[points.length - 1].y,
-            2
+            points[points.length - 1].y
           );
 
           // 線（LAN）が付いた画像を動かしたときに線も一緒に付いてくる動作の変数保存
@@ -155,11 +120,9 @@ $(function(){
 
           ctx.clearRect(0, 0, canvasWidth, canvasHeight);
           ctx.beginPath();
-          ctx.moveTo(array[0].x, array[0].y);
-          ctx.lineTo(array[1].x, array[1].y);
-          ctx.lineTo(array[2].x, array[2].y);
-          ctx.lineTo(array[3].x, array[3].y);
-          ctx.closePath();
+          ctx.moveTo(pointsFirst[0].x, pointsFirst[0].y);
+          ctx.lineTo(points[points.length - 1].x, points[points.length - 1].y);
+          ctx.stroke();
           ctx.fill();
         }
 
@@ -167,8 +130,7 @@ $(function(){
         nodeInt++;
 
         // Lanの長さが短いとき(1px分しかドラック) または フラグが合う時×2
-        // e.toElementが IE と FireFox だと null undefined になる（取得できない） 他の方法で取得すれば OK（たぶんないかも）
-        if(points.length == 1 || (lanFlag == false && lanFlag_point == true) || $(e.toElement).hasClass("lanFrist")) {
+        if(points.length == 1 || (lanFlag == false && lanFlag_point == true) || $(e.target).hasClass("lanFrist")) {
           nodeInt--;
           if (lnkFlag == true) { $(".sP_"+ nodeInt).removeClass("sP_"+ nodeInt); }
           else { $(".sP_"+ nodeInt).removeClass("lanLinck sP_"+ nodeInt); }
@@ -183,7 +145,6 @@ $(function(){
 
       $("#main").on("mousedown", lanDown);
       $("#main").on("mouseup", lanUp);
-
       canvasWidth = $('#main').width();
       canvasHeight = $('#main').height();
 
@@ -201,8 +162,8 @@ $(function(){
         });
         // 一時的にドラッグ機能を無効
         $(this).draggable("disable");
-
       }
+
       function lanBorder_off() {
         // フラグの設定
         lanFlag = false;
@@ -230,88 +191,79 @@ $(function(){
         $("#main").on("mousedown", lanDown2);
         $("#main").on("mouseup", lanUp2);
       }
-
     }
   });
 
-  // 線（LAN）が付いた画像を動かしたときに線も一緒に付いてくる動作
+  // マウスを押したとき、線（LAN）が付いた画像を動かしたときに線も一緒に付いてくる動作
   function lanDown2(e) {
-    if ($(e.toElement).hasClass("lanLinck")){
+    if ($(e.target).hasClass("lanLinck")){
       $(this).on("mousemove", lanMove2);
     }
   }
 
+  // ドラッグしている時、線（LAN）が付いた画像を動かしたときに線も一緒に付いてくる動作
   function lanMove2(e){
     for(var i=0; i < nodeInt; i++){
-      if ($(e.toElement).hasClass("sP_"+ i)){
+      if ($(e.target).hasClass("sP_"+ i)){
         var ctx = ctxArr[i];
         points.push({x:e.pageX - this.offsetLeft - e.offsetX + 38, y:e.pageY - this.offsetTop - e.offsetY + 38});
         var array = getRectPoints(
-          points[points.length - 1].x,  // 線Xの終点
-          points[points.length - 1].y,  // 線Yの終点
-          lanArr[i][1].x,               // 線Xの始点
-          lanArr[i][1].y,               // 線Yの始点
-          2                             // 線の太さ
+          points[points.length - 1].x,
+          points[points.length - 1].y,
+          lanArr[i][1].x,
+          lanArr[i][1].y
         );
         ctx.clearRect(0, 0, canvasWidth, canvasHeight);
         ctx.beginPath();
         ctx.moveTo(array[0].x, array[0].y);
         ctx.lineTo(array[1].x, array[1].y);
-        ctx.lineTo(array[2].x, array[2].y);
-        ctx.lineTo(array[3].x, array[3].y);　
-        ctx.closePath();
+        ctx.stroke();
         ctx.fill();
       }
-      else if ($(e.toElement).hasClass("eP_"+ i)){
+      else if ($(e.target).hasClass("eP_"+ i)){
         var ctx = ctxArr[i];
         points.push({x:e.pageX - this.offsetLeft - e.offsetX + 38, y:e.pageY - this.offsetTop - e.offsetY + 38});
         var array = getRectPoints(
-          lanArr[i][0].x,               // 線Xの始点
-          lanArr[i][0].y,               // 線Yの始点
-          points[points.length - 1].x,  // 線Xの終点
-          points[points.length - 1].y,  // 線Yの終点
-          2                             // 線の太さ
+          lanArr[i][0].x,
+          lanArr[i][0].y,
+          points[points.length - 1].x,
+          points[points.length - 1].y
         );
         ctx.clearRect(0, 0, canvasWidth, canvasHeight);
         ctx.beginPath();
         ctx.moveTo(array[0].x, array[0].y);
         ctx.lineTo(array[1].x, array[1].y);
-        ctx.lineTo(array[2].x, array[2].y);
-        ctx.lineTo(array[3].x, array[3].y);　
-        ctx.closePath();
+        ctx.stroke();
         ctx.fill();
       }
     }
   }
 
+  // マウスを放した時、線（LAN）が付いた画像を動かしたときに線も一緒に付いてくる動作
   function lanUp2(e){
-    for(var i=0; i < nodeInt; i++){
-      if ($(e.toElement).hasClass("sP_"+ i)){
-        var array = getRectPoints(
-          points[points.length - 1].x,  // 線Xの始点
-          points[points.length - 1].y,  // 線Yの始点
-          lanArr[i][1].x,               // 線Xの終点
-          lanArr[i][1].y,               // 線Yの終点
-          2                             // 線の太さ
-        );
-        if ( !(array === undefined) ){
+    // ドラックした時
+    if(!(points.length == 0)){
+      for(var i=0; i < nodeInt; i++){
+        if ($(e.target).hasClass("sP_"+ i)){
+          var array = getRectPoints(
+            points[points.length - 1].x,
+            points[points.length - 1].y,
+            lanArr[i][1].x,
+            lanArr[i][1].y
+          );
           lanArr[i] = array;
         }
-      }
-      else if ($(e.toElement).hasClass("eP_"+ i)){
-        var array = getRectPoints(
-          lanArr[i][0].x,               // 線Xの始点
-          lanArr[i][0].y,               // 線Yの始点
-          points[points.length - 1].x,  // 線Xの終点
-          points[points.length - 1].y,  // 線Yの終点
-          2                             // 線の太さ
-        );
-        if ( !(array === undefined) ){
+        else if ($(e.target).hasClass("eP_"+ i)){
+          var array = getRectPoints(
+            lanArr[i][0].x,
+            lanArr[i][0].y,
+            points[points.length - 1].x,
+            points[points.length - 1].y
+          );
           lanArr[i] = array;
         }
       }
     }
-
     $(this).off("mousemove", lanMove2);
     points = [];
   }
